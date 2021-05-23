4d959cacb8d54510d6ebe715f88d4fa1c5de8726
SonaBabu999 <mca550@rajagiri.edu>
2021-05-23 10:08:01 +0530

    fork

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Honest Bakers') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -36,7 +36,7 @@
                 <a class="navbar-brand" href="/management">
                 <i class="fas fa-utensils"></i>
 
-{{ config('app.name', 'Laravel') }}
+{{ config('app.name', 'Honest Bakers') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
