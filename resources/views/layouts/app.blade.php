804c0ae38fbd7b061fe779d145e872b63b403193
SonaBabu999 <mca550@rajagiri.edu>
2021-05-23 17:20:16 +0530

    img

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,7 @@
                 <a class="navbar-brand" href="/management">
                 <i class="fas fa-utensils"></i>
 
-{{ config('app.name', 'Laravel') }}
+{{ config('app.name', 'HonestBakers') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
