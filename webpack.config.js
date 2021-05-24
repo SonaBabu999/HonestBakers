7a628bbab7a5e2cbbb06ed2d6fc72e056a05ac7f
SonaBabu999 <mca550@rajagiri.edu>
2021-05-24 12:14:05 +0530

    json added

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const { assertSupportedNodeVersion } = require('../src/Engine');
+
+module.exports = async () => {
+    assertSupportedNodeVersion();
+
+    const mix = require('../src/Mix').primary;
+
+    require(mix.paths.mix());
+
+    await mix.installDependencies();
+    await mix.init();
+
+    return mix.build();
+};
+module.exports = {
+    // ...
+    entry: {
+      "app": "src/app.js"
+    },
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: "[name].js",
+      sourceMapFilename: "[name].js.map"
+    },
+    devtool: "source-map"
+    // ...
+};
+
+    
