1661f24a1601d7c08609edafcc01c4320d8ea6b2
SonaBabu999 <mca550@rajagiri.edu>
2021-05-24 17:05:34 +0530

    config

--- webpack.config.js
+++ webpack.config.js
@@ -12,20 +12,52 @@ module.exports = async () => {
 
     return mix.build();
 };
+// module.exports = {
+//     // ...
+//     entry: {
+//       "app": "src/app.js"
+//     },
+//     output: {
+//       path: path.join(__dirname, 'dist'),
+//       filename: "[name].js",
+//       sourceMapFilename: "[name].js.map"
+//     },
+//     devtool: "source-map"
+//     // ...
+// };
+// module.exports = {
+//     mode: 'production'
+//   };
+    // webpack.config.js
+const { VueLoaderPlugin } = require('vue-loader')
+
 module.exports = {
-    // ...
-    entry: {
-      "app": "src/app.js"
-    },
-    output: {
-      path: path.join(__dirname, 'dist'),
-      filename: "[name].js",
-      sourceMapFilename: "[name].js.map"
-    },
-    devtool: "source-map"
-    // ...
-};
-module.exports = {
-    mode: 'production'
-  };
-    
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      // this will apply to both plain `.js` files
+      // AND `<script>` blocks in `.vue` files
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      // this will apply to both plain `.css` files
+      // AND `<style>` blocks in `.vue` files
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // make sure to include the plugin for the magic
+    new VueLoaderPlugin()
+  ]
+}
